75b5ae81c6f567b6b04d55328e73a3bab736bafd
tarunkalal-kpmg <tarun.kalal@kpmg.co.uk>
2019-03-10 10:32:02 +0000

    Initial Check-in
    Initial Check-in

---
+++ WpfSquirrelTestProject/AutoUpdateManager.cs
@@ -0,0 +1,46 @@
+﻿using Squirrel;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfSquirrelTestProject
+{
+    public class AutoUpdateManager : IDisposable
+    {
+        private const string AppName = "WpfSquirrelTestProject";
+        private const string ExeName = AppName + ".exe";
+
+        private readonly IUpdateManager  _updateManager;
+
+        public AutoUpdateManager(string updateUrl)
+        {
+            _updateManager = UpdateManager.GitHubUpdateManager(updateUrl).Result;
+        }
+       
+        public async Task CheckForUpdate()
+        {
+           await InvokeUpdate();
+        }
+
+        public void Dispose()
+        {
+            _updateManager.Dispose();
+        }
+
+        private async Task InvokeUpdate()
+        {
+            var updateInfo = await _updateManager.CheckForUpdate();
+            var lastVersion = updateInfo?.ReleasesToApply?.OrderBy(x => x.Version).LastOrDefault();
+            if (lastVersion == null) return;
+            if (MessageBox.Show($"An update to version {lastVersion.Version} is available. Do you want to update?",
+                           "Update available", MessageBoxButton.OKCancel) != MessageBoxResult.OK)
+            {
+                return;
+            }
+            await _updateManager.DownloadReleases(new[] { lastVersion });
+            await _updateManager.ApplyReleases(updateInfo);
+            await _updateManager.UpdateApp();
+        }
+    }
+}
---
+++ WpfSquirrelTestProject/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Squirrel;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfSquirrelTestProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string updatePath = @"C:\KPMG-TaxUK\WpfSquirrelTestProject\SquirrelPackage";
+        public MainWindow()
+        {
+            InitializeComponent();
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            ExeAssLoc.Text = assembly.Location;
+            Vernum.Text = assembly.GetName().Version.ToString(3);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                using (var _autoUpdateManager = new AutoUpdateManager(updatePath))
+                {
+                    await _autoUpdateManager?.CheckForUpdate();
+                }
+            });
+        }
+       
+    }
+}
