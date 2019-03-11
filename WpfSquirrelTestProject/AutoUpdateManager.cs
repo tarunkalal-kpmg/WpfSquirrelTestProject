68fd73eaf209383e92d326eaededa3a372ac2e8a
tarunkalal-kpmg <tarun.kalal@kpmg.co.uk>
2019-03-11 06:59:32 +0000

    AutoUpdateManager implemented. and version changed to 1..0.0

--- WpfSquirrelTestProject/AutoUpdateManager.cs
+++ WpfSquirrelTestProject/AutoUpdateManager.cs
@@ -9,38 +9,42 @@ namespace WpfSquirrelTestProject
     public class AutoUpdateManager : IDisposable
     {
         private const string AppName = "WpfSquirrelTestProject";
-        private const string ExeName = AppName + ".exe";
-
-        private readonly IUpdateManager  _updateManager;
+        private IUpdateManager  _updateManager;
+        private readonly string updatePath;
 
         public AutoUpdateManager(string updateUrl)
         {
-            _updateManager = UpdateManager.GitHubUpdateManager(updateUrl).Result;
-        }
-       
-        public async Task CheckForUpdate()
-        {
-           await InvokeUpdate();
+            updatePath = updateUrl;
         }
 
         public void Dispose()
         {
-            _updateManager.Dispose();
+            _updateManager?.Dispose();
         }
-
-        private async Task InvokeUpdate()
+        public async Task InvokeUpdate()
         {
-            var updateInfo = await _updateManager.CheckForUpdate();
-            var lastVersion = updateInfo?.ReleasesToApply?.OrderBy(x => x.Version).LastOrDefault();
-            if (lastVersion == null) return;
-            if (MessageBox.Show($"An update to version {lastVersion.Version} is available. Do you want to update?",
-                           "Update available", MessageBoxButton.OKCancel) != MessageBoxResult.OK)
+            try
             {
-                return;
+                _updateManager = await UpdateManager.GitHubUpdateManager(updatePath, AppName);
+                var updates = await _updateManager.CheckForUpdate();
+                var lastVersion = updates?.ReleasesToApply?.OrderBy(x => x.Version).LastOrDefault();
+                if (lastVersion == null) return;
+
+                if (MessageBox.Show($"An update to version {lastVersion.Version} is available. Do you want to update?",
+                        "Update available", MessageBoxButton.OKCancel) != MessageBoxResult.OK)
+                {
+                    return;
+                }
+
+                await _updateManager.DownloadReleases(new[] { lastVersion });
+                await _updateManager.ApplyReleases(updates);
+                await _updateManager.UpdateApp();
+                MessageBox.Show("The application has been updated - please close and restart.");
             }
-            await _updateManager.DownloadReleases(new[] { lastVersion });
-            await _updateManager.ApplyReleases(updateInfo);
-            await _updateManager.UpdateApp();
+            catch (Exception e)
+            {
+                MessageBox.Show("Update check failed with an exception: " + e.Message);
+            }
         }
     }
 }
--- WpfSquirrelTestProject/MainWindow.xaml.cs
+++ WpfSquirrelTestProject/MainWindow.xaml.cs
@@ -23,41 +23,13 @@ namespace WpfSquirrelTestProject
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            UpdateApp().ConfigureAwait(false);
-
-            MessageBox.Show("Finish");
-        }
-
-        public async Task UpdateApp()
-        {
-            try
+            Task.Run(async () =>
             {
-                var mgr = await UpdateManager.GitHubUpdateManager(updatePath, "WpfSquirrelTestProject");
-                var updates = await mgr.CheckForUpdate();
-                var lastVersion = updates?.ReleasesToApply?.OrderBy(x => x.Version).LastOrDefault();
-                if (lastVersion == null)
+                using (var autoUpdater = new AutoUpdateManager(updatePath))
                 {
-                    MessageBox.Show("No Updates are available at this time.");
-                    return;
+                    await autoUpdater.InvokeUpdate().ConfigureAwait(false);
                 }
-
-                if (MessageBox.Show($"An update to version {lastVersion.Version} is available. Do you want to update?",
-                        "Update available", MessageBoxButton.OKCancel) != MessageBoxResult.OK)
-                {
-                    return;
-                }
-                MessageBox.Show("DEBUG: Don't actually perform the update in debug mode");
-
-                await mgr.DownloadReleases(new[] { lastVersion });
-                await mgr.ApplyReleases(updates);
-                await mgr.UpdateApp();
-
-                MessageBox.Show("The application has been updated - please close and restart.");
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show("Update check failed with an exception: " + e.Message);
-            }
+            });
         }
     }
 }
